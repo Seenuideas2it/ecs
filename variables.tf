0c063f5e82c3fa99cd853031e8b8a25cbc0b6335
Seenuideas2it <87300098+Seenuideas2it@users.noreply.github.com>
2021-08-09 12:31:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "az_count" {
 }
 
 variable "ecs_task_execution_role" {
-  default     = "myECcsTaskExecutionRole"
+  default     = "myECS TaskExecutionRole"
   description = "ECS task execution role name"
 }
 
