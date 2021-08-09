d2857a01e00946e6eac20b993bcfff3587bab278
Seenuideas2it <87300098+Seenuideas2it@users.noreply.github.com>
2021-08-09 22:22:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  default     = "ca-central-1"
+  default     = "us-west-1"
   description = "aws region where our resources going to create choose"
   #replace the region as suits for your requirement
 }
